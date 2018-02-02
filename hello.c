f57ad03c0406e424940e0bdd2f9fc19192132b11
yekai1003 <yekai_23@sohu.com>
2018-02-02 15:17:23 +0800

    add hello.c

---
+++ hello.c
@@ -0,0 +1,20 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: YeKai
+    > Company: www.itcast.cn 
+    > Created Time: Fri 02 Feb 2018 03:15:28 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+int main(int argc,char *argv[])
+{
+    
+    return 0;
+}
